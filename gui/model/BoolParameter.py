787d51d99ee327d25046301ddf9c343118918004
Vlad Negara <150728638+VladNegara@users.noreply.github.com>
2026-02-25 09:36:06 +0100

    Fix type annotations

--- gui/model/BoolParameter.py
+++ gui/model/BoolParameter.py
@@ -1,7 +1,7 @@
 from model.Parameter import Parameter
 
 class BoolParameter(Parameter[bool]):
-    def get_flag_with_value(self):
+    def get_flag_with_value(self) -> str:
         if self.value:
             return self.flag
         else:
