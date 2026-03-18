486e73788c7900f78866cd1a3d1962f318c08a96
parrot <s.h.broeder@student.utwente.nl>
2026-03-18 10:56:21 +0000

    Add documentation to StatusBar

--- gui/widgets/status_bar.py
+++ gui/widgets/status_bar.py
@@ -1,33 +1,32 @@
 from PySide6.QtCore import (
-    Signal,
+    Slot,
     QDir,
 )
 from PySide6.QtWidgets import (
-    QMainWindow,
-    QWidget,
-    QHBoxLayout,
-    QVBoxLayout,
-    QStackedLayout,
-    QScrollArea,
-    QPushButton,
     QLabel,
     QStatusBar,
 )
 
 from gui.model.settings import app_settings
 
-class StatusBar(QStatusBar):
-    workspace_path_changed = Signal(QDir)
 
+class StatusBar(QStatusBar):
+    """
+    Class to represent the status bar of the RAiSD-AI GUI application.
+    """
     def __init__(self) -> None:
+        """
+        Initialize the `StatusBar`.
+        """
         super().__init__()
         self.setStyleSheet("background-color: lightgray;")
 
+        # Label to show the current workspace path
         self.workspace_label = QLabel()
-        self.workspace_path_changed.connect(self.update_workspace_label)
         self.update_workspace_label(app_settings.workspace_path)
-
         self.addWidget(self.workspace_label)
 
+    @Slot(QDir)
     def update_workspace_label(self, new_workspace: QDir) -> None:
+        """Update the workspace label to show the current workspace path."""
         self.workspace_label.setText(f"Current Workspace: '{new_workspace.absolutePath()}'")
