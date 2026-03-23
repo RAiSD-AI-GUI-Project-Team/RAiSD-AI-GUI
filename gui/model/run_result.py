81ab8c71c6e411d02760f388e686ce8769073d9d
Loes Baart de la Faille <l.baartdelafaille@student.utwente.nl>
2026-03-23 13:35:19 +0000

    Implement retrieving history from file

--- gui/model/run_result.py
+++ gui/model/run_result.py
@@ -3,31 +3,85 @@ from gui.model.parameter_group_list import ParameterGroupList
 from PySide6.QtCore import QDir
 
 import json
-import datetime
+from datetime import datetime 
 from gui.model.settings import app_settings
 
 class RunResult():
     def __init__(
             self, 
             name: str = "",
+            commands: list[str] | None = None,
+            parameter_group_list: ParameterGroupList = None,
+            time_completed: datetime | None = None
         ):
         self._folder_name = name
-        self._commands = None
-        self._parameter_group_list = ParameterGroupList.from_yaml(app_settings.yaml_path)
-        self._time_completed = None
+        self._commands = commands
+        self._parameter_group_list = parameter_group_list or ParameterGroupList.from_yaml(app_settings.yaml_path)
+        self._time_completed = time_completed
 
     @classmethod
-    def from_history_file(cls) -> list["RunResult"]:
-        with open(app_settings.workspace_path.absoluteFilePath("history.json"), "r") as f:
-            try:
-                data = json.loads(f)
-            except:
-                pass
+    def from_history_file(cls) -> list["RunResult"] | None:
+        run_results = []
+        try: 
+            with open(app_settings.workspace_path.absoluteFilePath("history.json"), "r") as f:
+                data = json.load(f)
+                if not isinstance(data, dict):
+                    raise json.JSONDecodeError
+                for key in data.keys():
+                    if not isinstance(data[key], dict):
+                        raise json.JSONDecodeError
+                    run_results.append(cls.from_dict(data[key]))
+                return run_results
+        except FileNotFoundError:
+            print("No history file found in this workspace")
+            return None
+        except json.JSONDecodeError:
+            print("History file not parseable. Might be empty or formatted incorrectly")
+            return []
+            
 
     @classmethod
-    def from_str(cls, string: str) -> "RunResult":
-        #TODO: implement parsing
-        pass
+    def from_dict(cls, dictionary: dict) -> "RunResult":
+        name = dictionary.get("folder_name")
+        if not isinstance(name, str):
+            raise ValueError(
+                f"Invalid run name: {name}. "
+                + "Expected string name."
+            )
+
+        commands = dictionary.get("commands")
+        if not isinstance(commands, list):
+            raise ValueError(
+                f"Invalid commands object: {commands}. "
+                + "Expected list."
+            )
+        
+        for command in commands:
+            if not isinstance(command, str):
+                raise ValueError(
+                    f"Invalid command type: {command}"
+                    + "Expected string."
+                )
+        
+        parameters = dictionary.get("parameters")
+        if not isinstance(parameters, dict):
+            raise ValueError(
+                f"Invalid parameter object: {parameters}."
+                + "Expected dictionary."
+            )
+        
+        parameter_group_list = ParameterGroupList.from_yaml(app_settings.yaml_path)
+        # TODO populate with parameter values
+
+        time_completed = dictionary.get("time_completed")
+        if not isinstance(time_completed, str):
+            raise ValueError(
+                f"Invalid time_completed type: {time_completed}"
+                + "Expected string."
+            )
+        time_completed = datetime.strptime(time_completed, "%Y-%m-%d %H:%M:%S.%f")
+
+        return cls(name, commands, parameter_group_list, time_completed)
 
     def populate_parameter_group_list(
             self, 
@@ -47,7 +101,7 @@ class RunResult():
             "folder_name": self.folder_name,
             "commands": self._commands,
             "parameters": parameters_dict,
-            "time_completed": datetime.datetime.now()
+            "time_completed": self._time_completed
         }
         return dict
 
