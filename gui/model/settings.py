d838f68c138f80f6e44a612da87b7afc0b81546a
Loes Baart de la Faille <l.baartdelafaille@student.utwente.nl>
2026-03-23 13:35:56 +0000

    Add yaml path attribute in Settings

--- gui/model/settings.py
+++ gui/model/settings.py
@@ -42,6 +42,7 @@ class Settings(QObject):
         self._executable_file_path = QFileInfo()
         self._environment_manager = EnvironmentManager.MICROMAMBA
         self._environment_name = ""
+        self._yaml_path = "gui/config.yaml"
 
     @property
     def workspace_path(self) -> QDir:
@@ -130,6 +131,14 @@ class Settings(QObject):
         if self._environment_name != value:
             self._environment_name = value
             self.environment_name_changed.emit(value)
+
+    @property
+    def yaml_path(self) -> str:
+        return self._yaml_path
+    
+    @yaml_path.setter
+    def yaml_path(self, path: str) -> None:
+        self._yaml_path = path
 
     def set_workspace_folder(self) -> None:
         """
