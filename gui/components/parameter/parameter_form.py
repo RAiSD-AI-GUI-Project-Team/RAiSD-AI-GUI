6fc289825a4dc6b2eb380337253762d3a58d6a5e
alphanmete <alphanmete22@gmail.com>
2026-04-06 15:43:12 +0200

    Introduce functions to set invalid boolean for parameter_form_sections, update the invalid sections once their parameters become valid and clear the validity_hints when navigating

--- gui/components/parameter/parameter_form.py
+++ gui/components/parameter/parameter_form.py
@@ -26,7 +26,7 @@ class ParameterForm(QWidget):
         super().__init__()
         self._editable = editable
         self._run_record = run_record
-        self._parameter_form_sections = []
+        self._parameter_form_sections: list[ParameterFormSection] = []
         layout = QVBoxLayout(self)
 
         heading = QLabel("RAiSD-AI parameters")
@@ -46,3 +46,40 @@ class ParameterForm(QWidget):
     def untouch_all(self) -> None:
         for section in self._parameter_form_sections:
             section.untouch_all()
+
+    def update_validity_hints(self) -> None:
+        """
+        Update each section's border based on whether it contains any invalid enabled parameters.
+        """
+        for section in self._parameter_form_sections:
+            section_invalid = self._section_with_invalid_parameters(section)
+            section.set_invalid(section_invalid)
+
+    def update_active_hints(self) -> None:
+        """
+        Change invalid(red) sections to valid sections
+        when the respective invalid parameters are changed to valid
+        """
+        for section in self._parameter_form_sections:
+            has_invalid_property = section.property("invalid") == "true"
+            if has_invalid_property:
+                section_invalid = self._section_with_invalid_parameters(section)
+                if not section_invalid:
+                    section.set_invalid(False)
+
+    def clear_validity_hints(self) -> None:
+        """
+        Clear red border from all sections when navigating
+        """
+        for section in self._parameter_form_sections:
+            section.set_invalid(False)
+
+    def _section_with_invalid_parameters(self, section) -> bool:
+        """
+        Helper function that returns true for a section with an invalid parameters
+        Otherwise, returns false
+        """
+        for parameter in section.parameter_group.parameters:
+            if parameter.enabled and not parameter.valid:
+                return True
+        return False
