8f3fb54812db6286c7af339f8903f6d5ae1d2d28
parrot <s.h.broeder@student.utwente.nl>
2026-04-14 09:26:24 +0000

    Create init test

--- gui/tests/unit/model/test_settings.py
+++ gui/tests/unit/model/test_settings.py
@@ -1,5 +1,11 @@
 import pytest
 
+from PySide6.QtCore import (
+    QDir,
+    QFileInfo
+)
+
+from gui.model.settings import Settings, app_settings
 
 class TestSettings:
     """Tests for Settings class."""
@@ -10,10 +16,27 @@ class TestSettings:
 
     def test_init_values(self):
         """Test Settings."""
-        # TODO: Implement this testing class
         # Arrange
+        workspace_path = QDir("/home/raisd")
+        executable_file_path = QFileInfo("/home/raisd/raisdai")
+        environment_manger = 0
+        environment_manger_name = Settings.environment_managers[environment_manger]
+        environment_name = "raisdai"
+        config_path = QFileInfo("/home/raisd/config.yml")
 
         # Act
+        settings = Settings(
+            workspace_path=workspace_path,
+            executable_file_path=executable_file_path,
+            environment_manager=environment_manger,
+            environment_name=environment_name,
+            config_path=config_path,
+        )
 
         # Assert
-        pytest.skip()
+        assert settings.workspace_path == workspace_path
+        assert settings.executable_file_path == executable_file_path
+        assert settings.environment_manager == settings.environment_manager
+        assert settings.environment_manager_name == environment_manger_name
+        assert settings.environment_name == environment_name
+        assert settings.config_path == config_path
